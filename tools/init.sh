ec19c5eef2593b441d8840dc95eb0a48341d486e
jieun-b <qwldms25@gmail.com>
2025-03-19 02:40:36 +0900

    Modify

--- tools/init.sh
+++ tools/init.sh
@@ -9,9 +9,6 @@ CLI=("git" "npm")
 
 ACTIONS_WORKFLOW=pages-deploy.yml
 
-# Get the latest release hash, if available
-RELEASE_HASH=$(git log --grep="chore(release):" -1 --pretty="%H" || echo "")
-
 # temporary file suffixes that make `sed -i` compatible with BSD and Linux
 TEMP_SUFFIX="to-delete"
 
@@ -52,26 +49,13 @@ _check_status() {
   fi
 }
 
-_check_init() {
-  if [[ -z "$RELEASE_HASH" ]]; then
-    echo "No release commit found."
-    exit 1
-  fi
-
-  if [[ $(git rev-parse HEAD^1) == "$RELEASE_HASH" ]]; then
-    echo "Already initialized."
-    exit 0
-  fi
-}
-
 check_env() {
   _check_cli
   _check_status
-  _check_init
 }
 
 reset_latest() {
-  git reset --hard "$RELEASE_HASH"
+  git reset --hard HEAD
   git clean -fd
   git submodule update --init --recursive
 }
