b2cda7d020f48e106c747fc694ce07b103d80d0f
jieun-b <qwldms25@gmail.com>
2025-03-19 02:35:51 +0900

    Modify

--- tools/init.sh
+++ tools/init.sh
@@ -96,17 +96,10 @@ init_files() {
   _sedi "/^assets\/js\/dist/d" .gitignore
 }
 
-# commit() {
-#   git add -A
-#   git commit -m "chore: initialize the environment" -q
-#   echo -e "\n> Initialization successful!\n"
-# }
-
 main() {
   check_env
   reset_latest
   init_files
-  # commit
 }
 
 while (($#)); do
